c455a0f9b363bc047d851f47717f2d414029a2d3
Fuchigami KOICHI <e235714@ie.u-ryukyu.ac.jp>
2023-11-15 00:17:31 +0900

    3rd step commit

--- app/src/test/java/jp/ac/uryukyu/ie/e235714/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e235714/EnemyTest.java
@@ -16,7 +16,7 @@ class EnemyTest {
             demoHero.attack(slime); // over kill
         }
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
 
